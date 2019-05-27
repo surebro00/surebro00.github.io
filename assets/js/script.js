aa9772d1d9fabadaf6f9561a178bbc1d7dd1aa53
surebro00 <36013049+surebro00@users.noreply.github.com>
2019-05-28 01:12:02 +0200

    add pages

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#more").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#more_start").offset().top},
+            'slow');
+    });
+
+    $(".link_1").click(function() {
+        $('html,body').animate({
+                scrollTop: $("#more_start").offset().top},
+            'slow');
+    });
+});
