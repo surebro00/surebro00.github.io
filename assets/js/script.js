8acdd501cc3e41d1faa5435be965fe012531d265
surebro00 <36013049+surebro00@users.noreply.github.com>
2019-05-31 13:13:25 +0200

    added cookie bar and some fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,19 @@ $(document).ready(function () {
             'slow');
     });
 
+    $.cookieBar({
+        message: 'We don\'t use cookies but we suggest to read this copyright notice',
+        acceptButton: true,
+        acceptText: 'Ok, done',
+        policyButton: true,
+        policyText: 'Read more',
+        policyURL: 'copyright-notify.html',
+        effect: 'slide',
+        fixed: true,
+        bottom: true,
+        zindex: '999',
+        domain: 'www.ripfred.it',
+        referrer: 'www.ripfred.it'
+    });
+
 });
